2297a4504dc559bd14c363ad08fd0b221d0d9bf2
Antonie Racanska <247294@vutbr.cz>
2023-04-17 09:37:07 +0200

    second_commit

--- sorting.py
+++ sorting.py
@@ -22,9 +22,33 @@ def read_data(file_name):
     return data
 
 
+##########
+
+def selection_sort(seznam):
+    """
+    :param seznam:
+    :return:
+    """
+    n = len(seznam)
+    for i in range(n):
+        min_idx = i
+        for number in range(i+1, n):
+            if seznam[number] < seznam[min_idx]:
+                min_idx = number
+        seznam[i], seznam[min_idx] = seznam[min_idx], seznam[i]
+    return seznam
+
+##########
+def bubble_sort(seznam_cisel):
+
+
+
+
 def main():
-    my_data = read_data('numbers.csv')
-    print(my_data)
+   my_data = read_data('numbers.csv')
+   print(my_data['series_1'])
+   selection_sort_result = selection_sort(my_data['series_1'])
+   print(selection_sort_result)
 
 
 if __name__ == '__main__':
